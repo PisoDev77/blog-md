ff0be7eb011ad6db75c9afd7d5464bb013ed559a
PisoDev77 <pisodev0427@gmail.com>
2023-04-17 19:38:00 +0900

    style & main modified nth special

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,44 @@
-import tmp from "./articles.json";
 import "./style.css";
+import Navigation, { eventHandler as navEvent } from "./components/Nav";
+import Article, { eventHandler as articleEvent } from "./components/Article";
+import { eventHandler as menubtnEvent } from "./components/MenuBtn";
+import { articleType, currentContent } from "./type/article";
+
+const $app = document.getElementById("app") as HTMLDivElement;
 
 document.body.className = "markdown-body";
 
-const $nav = document.createElement("nav");
-$nav.innerHTML = `
-  <nav>
-    <h1>Piso's Blog<small>(${tmp.length})</small></h1>
-  </nav>
-`;
+const currents: { currentArticles: articleType[]; menus: boolean } = {
+  currentArticles: [],
+  menus: false,
+};
 
-const $article = document.createElement("article");
+const articleProxy = new Proxy(currents, {
+  set(target: any, key: string, value: any) {
+    target[key] = value;
+    render(key === "currentArticles" ? value : currents.currentArticles);
+    return true;
+  },
+});
 
-document.body.innerHTML = $nav.innerHTML + $article.innerHTML;
+$app.addEventListener("click", (event: MouseEvent) => {
+  const nav = navEvent(event);
+  if (nav) articleProxy.currentArticles = nav;
+  articleProxy.menus = false;
+});
+
+$app.addEventListener("click", (event: MouseEvent) => {
+  const art = articleEvent(event);
+  if (art) articleProxy.currentArticles = articleEvent(event);
+  articleProxy.menus = false;
+});
+
+$app.addEventListener("click", (event) => {
+  articleProxy.menus = menubtnEvent(event);
+});
+
+const render = (article: currentContent) => {
+  $app.innerHTML = Navigation(currents.menus) + Article(article);
+};
+
+render(currents.currentArticles);
