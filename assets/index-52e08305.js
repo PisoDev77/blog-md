6a034d1ba480d486b66e82ab4737fecd5b25bb64
PisoDev77 <pisodev0427@gmail.com>
2023-04-18 07:56:16 +0900

    Updates

--- assets/index-52e08305.js
+++ assets/index-52e08305.js
@@ -1,4 +1,4 @@
-(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const r of document.querySelectorAll('link[rel="modulepreload"]'))c(r);new MutationObserver(r=>{for(const o of r)if(o.type==="childList")for(const d of o.addedNodes)d.tagName==="LINK"&&d.rel==="modulepreload"&&c(d)}).observe(document,{childList:!0,subtree:!0});function n(r){const o={};return r.integrity&&(o.integrity=r.integrity),r.referrerPolicy&&(o.referrerPolicy=r.referrerPolicy),r.crossOrigin==="use-credentials"?o.credentials="include":r.crossOrigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function c(r){if(r.ep)return;r.ep=!0;const o=n(r);fetch(r.href,o)}})();const f="menu-btn";function p(){return`<i class="${f} bx bx-menu"></i>`}const y=e=>!!e.target.classList.contains(f),l=[{id:"53c91411-c04f-4cb8-97db-fba50cdaaf51",category:"javascript",title:"Debounce-VS-Throttle",date:1681771646346,content:`<h1 id="debounce-디바운스--vs-throttle-쓰로틀-">Debounce( 디바운스 ) VS Throttle( 쓰로틀 )</h1>
+(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const r of document.querySelectorAll('link[rel="modulepreload"]'))c(r);new MutationObserver(r=>{for(const o of r)if(o.type==="childList")for(const d of o.addedNodes)d.tagName==="LINK"&&d.rel==="modulepreload"&&c(d)}).observe(document,{childList:!0,subtree:!0});function n(r){const o={};return r.integrity&&(o.integrity=r.integrity),r.referrerPolicy&&(o.referrerPolicy=r.referrerPolicy),r.crossOrigin==="use-credentials"?o.credentials="include":r.crossOrigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function c(r){if(r.ep)return;r.ep=!0;const o=n(r);fetch(r.href,o)}})();const f="menu-btn";function b(){return`<i class="${f} bx bx-menu"></i>`}const y=e=>!!e.target.classList.contains(f),l=[{id:"e50c6984-dc5d-4aaa-a46a-301c52a02c83",category:"javascript",title:"Debounce( 디바운스 ) VS Throttle( 쓰로틀 )",date:1681772113966,content:`<h1 id="debounce-디바운스--vs-throttle-쓰로틀-">Debounce( 디바운스 ) VS Throttle( 쓰로틀 )</h1>
 <h2 id="debounce--디바운스-">Debounce ( 디바운스 )</h2>
 <h3 id="구글-인프런-네이버-등-많은-웹-사이트에서-검색을-해보신-적-있나요">구글, 인프런, 네이버 등 많은 웹 사이트에서 검색을 해보신 적 있나요?</h3>
 <ul>
@@ -42,7 +42,7 @@
 <ul>
 <li>모든 이벤트를 감지하는 중, 특정 시간 이후에 <code>한 번만 원하는 동작</code>이 실행되도록 하는 기법이다.</li>
 </ul>
-`},{id:"8a9bb4f1-6893-4dbd-a079-e34b749d88a5",category:"react",title:"",date:1681771646353,content:`<h2 id="리액트-카테고리의-글입니다">리액트 카테고리의 글입니다.</h2>
+`},{id:"acd4a6c2-1451-4b1a-92c4-59ae6ce60dee",category:"react",title:"test1",date:1681772113968,content:`<h2 id="리액트-카테고리의-글입니다">리액트 카테고리의 글입니다.</h2>
 `}],v=["javascript","react"],h=e=>[...e==="all"?l:l.filter(t=>t.category===e)],$=()=>l.length,L=()=>v,A=e=>l.find(n=>n.id+""===e),x=e=>e.replace(/^\w/,t=>t.toUpperCase());function B(e){return`
   <div class="modal">
   <ul class="menus ${e?"slidein":"slideout"}">
@@ -53,7 +53,7 @@
     </ul>
   </div>
   `}const m="blog-navigation";function w(e){return`
-  ${p()}
+  ${b()}
   <nav class="${m}"> 
     ${e?B(e):""}
   </nav>`}const C=e=>{const t=e.target;if(t.closest("."+m)){const n=t.dataset.category;return n?[...h(n)]:void 0}};function E(e){const{id:t,title:n}=e;return`
@@ -65,8 +65,8 @@
     <section">
       ${t}
     </section>
-  `}const b="content-body";function P(e){return`
-  <article class="${b}">${e!==void 0?(()=>{var n;return Array.isArray(e)?`
+  `}const g="content-body";function P(e){return`
+  <article class="${g}">${e!==void 0?(()=>{var n;return Array.isArray(e)?`
       <h2>${((n=e[0])==null?void 0:n.category)??""}</h2>
       ${e.map(c=>E(c)).join("")}`:N(e)})():""}</article>
-  `}const u=e=>{const t=e.target;if(t.closest("."+b)){const n=t.closest(".article-list-box");if(n)return A(n.dataset.id??"")}},a=document.getElementById("app");document.body.className="markdown-body";const s={currentArticles:[],menus:!1},i=new Proxy(s,{set(e,t,n){return e[t]=n,g(t==="currentArticles"?n:s.currentArticles),!0}});a.addEventListener("click",e=>{const t=C(e);t&&(i.currentArticles=t),i.menus=!1});a.addEventListener("click",e=>{u(e)&&(i.currentArticles=u(e)),i.menus=!1});a.addEventListener("click",e=>{i.menus=y(e)});const g=e=>{a.innerHTML=w(s.menus)+P(e)};window.addEventListener("DOMContentLoaded",()=>{g(s.currentArticles)});
+  `}const u=e=>{const t=e.target;if(t.closest("."+g)){const n=t.closest(".article-list-box");if(n)return A(n.dataset.id??"")}},a=document.getElementById("app");document.body.className="markdown-body";const s={currentArticles:[],menus:!1},i=new Proxy(s,{set(e,t,n){return e[t]=n,p(t==="currentArticles"?n:s.currentArticles),!0}});a.addEventListener("click",e=>{const t=C(e);t&&(i.currentArticles=t),i.menus=!1});a.addEventListener("click",e=>{u(e)&&(i.currentArticles=u(e)),i.menus=!1});a.addEventListener("click",e=>{i.menus=y(e)});const p=e=>{a.innerHTML=w(s.menus)+P(e)};window.addEventListener("DOMContentLoaded",()=>{p(s.currentArticles)});
