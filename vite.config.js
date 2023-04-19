120e93f3e5aecec6cd9b89c22bf9ec86cc2de8bf
PisoDev77 <pisodev0427@gmail.com>
2023-04-19 15:27:48 +0900

    fix: / 를 base 앞에 추가

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   // base: process.env.BASE_SRC,
-  base: "blog-md",
+  base: "/blog-md",
   build: {
     sourcemap: false,
   },
